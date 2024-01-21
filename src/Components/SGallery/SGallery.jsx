fbf570815fa414573066cd5180a1406fdec66aa5
jayannbal <jayannangela.balatero@tup.edu.ph>
2024-01-21 22:27:06 +0800

    update sgallery file

---
+++ src/Components/SGallery/SGallery.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import "./SGallery.css";
+import gallery_image from "../Assets/gallery1.png";
+import gallery_image2 from "../Assets/gallery2.png";
+import gallery_image3 from "../Assets/gallery3.png";
+
+const Gallery = () => {
+  return (
+    <div class="project">
+      <h1>Our Services</h1>
+      <h2>We've got what you need!</h2>
+      <div className="project-container">
+        <div className="project-box">
+          <div className="gallery_image">
+            <img src={gallery_image} alt="" />
+          </div>
+          <h3>Deep Living Room Cleaning</h3>
+          <p>
+            Always best after deep cleaning service to complete the whole
+            process 360 degrees cleaning...
+          </p>
+          <a href="#" class="btn">
+            Book Now
+          </a>
+        </div>
+
+        <div className="project-box">
+          <div className="gallery_image2">
+            <img src={gallery_image2} alt="" />
+          </div>
+          <h3>Deep Living Room Cleaning</h3>
+          <p>
+            Always best after deep cleaning service to complete the whole
+            process 360 degrees cleaning...
+          </p>
+          <a href="#" class="btn">
+            Book Now
+          </a>
+        </div>
+
+        <div className="project-box">
+          <div className="gallery_image3">
+            <img src={gallery_image3} alt="" />
+          </div>
+          <h3>Deep Bathroom Cleaning</h3>
+          <p>
+            Always best after deep cleaning service to complete the whole
+            process 360 degrees cleaning...
+          </p>
+          <a href="#" className="btn">
+            Book Now
+          </a>
+        </div>
+
+        <div className="project-box">
+          <div className="gallery_image">
+            <img src={gallery_image} alt="" />
+          </div>
+          <h3>Deep Living Room Cleaning</h3>
+          <p>
+            Always best after deep cleaning service to complete the whole
+            process 360 degrees cleaning...
+          </p>
+          <a href="#" class="btn">
+            Book Now
+          </a>
+        </div>
+
+        <div className="project-box">
+          <div className="gallery_image2">
+            <img src={gallery_image2} alt="" />
+          </div>
+          <h3>Deep Living Room Cleaning</h3>
+          <p>
+            Always best after deep cleaning service to complete the whole
+            process 360 degrees cleaning...
+          </p>
+          <a href="#" class="btn">
+            Book Now
+          </a>
+        </div>
+
+        <div className="project-box">
+          <div className="gallery_image3">
+            <img src={gallery_image3} alt="" />
+          </div>
+          <h3>Deep Bathroom Cleaning</h3>
+          <p>
+            Always best after deep cleaning service to complete the whole
+            process 360 degrees cleaning...
+          </p>
+          <a href="#" className="btn">
+            Book Now
+          </a>
+        </div>
+
+        <div className="more-projects-button">
+          <a href="#" className="btn">
+            More Services - Click Here
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
