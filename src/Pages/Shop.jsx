80f3117763646131b0f18c46f53a0cb16c5d55c1
jayannbal <jayannangela.balatero@tup.edu.ph>
2024-01-21 22:27:16 +0800

    update files

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../Components/Hero/Hero";
 import NewsLetter from "../Components/NewsLetter/NewsLetter";
 import Description from "../Components/Description/Description";
+import SGallery from "../Components/SGallery/SGallery";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
       <Description />
+      <SGallery />
       <NewsLetter />
     </div>
   );
