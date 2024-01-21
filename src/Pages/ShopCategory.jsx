76bf1140c82abb1e040653307b1bc79dfab174d2
acpr-2023 <144645620+acpr-2023@users.noreply.github.com>
2024-01-21 14:01:40 +0800

    updatedcode

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,10 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./CSS/ShopCategory.css";
 import { ShopContext } from "../Context/ShopContext";
 import Item from "../Components/Item/Item";
 
 const ShopCategory = (props) => {
-  const { all_product } = useContext(ShopContext);
+  const { all_product, addToCart } = useContext(ShopContext);
+  const [selectedItemId, setSelectedItemId] = useState(null);
+
+  const handleItemClick = (itemId) => {
+    setSelectedItemId(itemId);
+  };
+
+  const handleAddToCart = (itemId) => {
+    addToCart(itemId);
+    // Optionally, you can reset the selectedItemId after adding to the cart
+    setSelectedItemId(null);
+  };
 
   return (
     <div className="shop-category">
@@ -12,13 +23,19 @@ const ShopCategory = (props) => {
         {all_product.map((item, i) => {
           if (props.category === item.category) {
             return (
-              <Item
-                key={i}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                new_price={item.new_price}
-              />
+              <div key={i}>
+                <Item
+                  id={item.id}
+                  name={item.name}
+                  image={item.image}
+                  new_price={item.new_price}
+                  setSelectedItemId={handleItemClick}
+                />
+                {/* Add to Cart button for each item */}
+                <button onClick={() => handleAddToCart(item.id)}>
+                  ADD TO CART
+                </button>
+              </div>
             );
           } else {
             return null;
