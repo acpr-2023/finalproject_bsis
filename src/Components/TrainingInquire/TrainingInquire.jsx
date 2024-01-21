94df8ce6c525c4c8654b41d6910e8c7ec61e50ad
Eloissa <mariaeloissa.andal@tup.edu.ph>
2024-01-21 19:42:20 +0800

    trainings

--- src/Components/TrainingInquire/TrainingInquire.jsx
+++ src/Components/TrainingInquire/TrainingInquire.jsx
@@ -1,38 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import Typed from 'typed.js';
 
 import './TrainingInquire.css';
-import trainingHeader from "../Assets/trainingheader.jpg";
-import trainingHeader_2 from "../Assets/trainingheader_2.jpg";
-import trainingHeader_3 from "../Assets/trainingheader_3.jpg";
-import Trainers from "../Assets/trainingheader.jpg";
+import headerClean from "../Assets/headerClean.jpg";
+import trainingHeader_2 from "../Assets/CleaningService.png";
+import trainingHeader_3 from "../Assets/CleaningService_2.png";
+import basicCleaning from "../Assets/basiccleaning.jpg";
+import trainers from "../Assets/trainingheader.jpg";
+import green from "../Assets/green.jpg";
+import time from "../Assets/time.jpg";
 
 
 const TrainingInquire = () => {
-  const [shouldShowImageText, setShouldShowImageText] = useState(true);
+  const typedTarget = useRef(null);
 
-  const toggleImageTextVisibility = () => {
-    setShouldShowImageText(prevState => !prevState);
-  };
+  useEffect(() => {
+    const typed = new Typed(typedTarget.current, {
+      strings: ['Household Helper Training Program', 'Professional Cleaning Techniques', 'Advanced Home Care Skills'],
+      typeSpeed: 50,
+      backSpeed: 50,
+      backDelay: 1500,
+      loop: true
+    });
 
-  const ImageText = ({ imageUrl, title, description, buttonText, buttonLink }) => {
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  const ImageText = ({ imageUrl, title, description, additionalDescription, buttonText, buttonLink }) => {
+    const createMarkup = (htmlContent) => {
+      return { __html: htmlContent };
+    };
+  
     return (
       <div className="image-text">
         <img src={imageUrl} alt={title} />
         <h2>{title}</h2>
         <p>{description}</p>
+        <br/>
+        {additionalDescription && (
+          <p dangerouslySetInnerHTML={createMarkup(additionalDescription)}></p> // Render the additional description as HTML
+        )}
         <a href={buttonLink} className="button">{buttonText}</a>
       </div>
     );
   };
-
+  
+  
   return (
     <main id="MainContent" className="content-for-layout focus-none" role="main" tabIndex="-1">
-      <button onClick={toggleImageTextVisibility} className="toggle-button">
-        {shouldShowImageText ? 'Hide Details' : 'Show Details'}
-      </button>
-
       <section>
         <Swiper
           spaceBetween={50}
@@ -44,43 +63,64 @@ const TrainingInquire = () => {
           }}
         >
           <SwiperSlide>
-            <img src={trainingHeader} alt="Slide 1" />
+            <img src={headerClean} alt="Slide 1" />
           </SwiperSlide>
           <SwiperSlide>
             <img src={trainingHeader_2} alt="Slide 2" />
           </SwiperSlide>
-             <img src={trainingHeader_3} alt="Slide 2" />
+          <SwiperSlide>
+            <img src={trainingHeader_3} alt="Slide 3" />
+          </SwiperSlide>
         </Swiper>
       </section>
 
       <section>
-        <div className="rich-text content-container color-background-1 gradient rich-text--full-width content-container--full-width">
-          <div className="rich-text__wrapper rich-text__wrapper--center page-width">
-            <div className="rich-text__blocks center">
-              <h2 className="rich-text__heading rte h1">
-                <span>Household Helper Training Program</span>
-              </h2>
-            </div>
-          </div>
+        <h2 className="rich-text__heading" ref={typedTarget}>
+          {/* Typed.js will insert text here */}
+        </h2>
+      </section>
+
+      <section>
+        <div className="image-text-container">
+            <ImageText 
+                imageUrl={basicCleaning}
+                title="Basic Cleaning Skills Program"
+                description="This program is designed for individuals who want to master the foundational skills of household cleaning."
+                additionalDescription="<b>Hands-On Training:</b> Participants will practice cleaning various surfaces, learn to use different cleaning tools, and develop efficient cleaning routines.</span>"
+                buttonText="Inquire Now"
+                buttonLink="https://m.me/HappyHelpersManila"
+            />
+
+            <ImageText
+                imageUrl={trainers}
+                title="Advanced Specialty Cleaning Program"
+                description="Ideal for those who want to specialize in tackling specific cleaning challenges and maintaining specialized areas."
+                additionalDescription="<b>Hands-On Training:</b> Participants will gain hands-on experience in dealing with specialized cleaning tasks, using advanced cleaning equipment, and handling delicate surfaces.</span>"
+                buttonText="Inquire Now"
+                buttonLink="https://m.me/HappyHelpersManila"
+            />
+            <ImageText
+                imageUrl={green}
+                title="Green Cleaning and Eco-Friendly Practices Program"
+                description="This program focuses on environmentally friendly cleaning methods and sustainable practices."
+                additionalDescription="<b>Hands-On Training:</b> Participants will learn to identify and use eco-friendly cleaning products, implement sustainable cleaning practices, and create their own green cleaning solutions.</span>"
+                buttonText="Inquire Now"
+                buttonLink="https://m.me/HappyHelpersManila"
+            />
+            <ImageText
+                imageUrl={time}
+                title="Time Management and Organization Program"
+                description="Tailored for clients looking to optimize their cleaning routines, balancing efficiency with thoroughness."
+                additionalDescription="<b>Hands-On Training:</b> Participants will work on developing personalized cleaning schedules, organizing spaces effectively, and mastering time-saving cleaning techniques.</span>"
+                buttonText="Inquire Now"
+                buttonLink="https://m.me/HappyHelpersManila"
+            />
         </div>
       </section>
 
-      {shouldShowImageText && (
-        <section>
-        <ImageText 
-          imageUrl={Trainers}
-          title="Learn the Happy Helpers method of cleaning and disinfecting"
-          description="Professionalize the skills of your household help in our comprehensive training workshops tailor-fit to your home's needs."
-          buttonText="Inquire Now"
-          buttonLink="https://m.me/HappyHelpersManila"
-        />
-        {/* Repeat ImageText for other sections as needed */}
-      </section>
-      
-      )}
       {/* Additional content sections can be added here if needed */}
     </main>
   );
 };
 
-export default TrainingInquire
+export default TrainingInquire;
