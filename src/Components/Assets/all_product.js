66bdd4eaea2b76f06b0ef9f15e9f994de64773fa
acpr-2023 <144645620+acpr-2023@users.noreply.github.com>
2024-01-21 20:53:20 +0800

    updated code

--- src/Components/Assets/all_product.js
+++ src/Components/Assets/all_product.js
@@ -14,60 +14,75 @@ import p12_img from "./product_12.png";
 let all_product = [
   {
     id: 1,
-    name: "Deep Cleaning Service",
+    name: "Deep Cleaning",
     category: "services",
     image: p1_img,
-    new_price: 50.0,
+    new_price: 2900,
+    description:
+      "A thorough cleaning service designed for residences and offices that are currently in use. This includes an extensive cleaning regimen covering bedrooms, bathrooms, kitchen, living room, dining, and common areas. The service begins with a team of two enthusiastic cleaners working for a duration of 5 hours per session. The number of cleaners can be adjusted based on the size of your house or condominium unit. The cleaning process involves sweeping, wiping down surfaces, sanitizing, and disinfecting to ensure a comprehensive and hygienic environment.",
   },
   {
     id: 2,
-    name: "Post Construction Cleaning Service",
+    name: "Post Construction Cleaning",
     category: "services",
     image: p2_img,
-    new_price: 85.0,
+    new_price: 2600,
+    description:
+      "We're here to assist you in getting your new home ready! Our all-encompassing cleaning service is tailored for newly constructed or renovated homes that are currently unoccupied. We'll eliminate any remaining dust, dirt, and debris from every surface and room.",
   },
   {
     id: 3,
-    name: "Move-in, Move-out Cleaning Service",
+    name: "Move-in, Move-out Cleaning",
     category: "services",
     image: p3_img,
-    new_price: 60.0,
+    new_price: 2700,
+    description:
+      "Let us help you as you move in and out of your home! Enjoy a comprehensive cleaning for occupied homes with minimal furnishings inside. ",
   },
   {
     id: 4,
-    name: "Subscription Cleaning Service",
+    name: "Disinfection Fogging",
     category: "services",
     image: p4_img,
-    new_price: 100.0,
+    new_price: 1900,
+    description:
+      "Applies a disinfectant solution that purifies the air and delicately covers surfaces in your environment, reaching difficult-to-access areas where potentially harmful germs and bacteria could be present.ed. ",
   },
   {
     id: 5,
-    name: "Upholstery Cleaning Service",
+    name: "Subscription Cleaning",
     category: "services",
     image: p5_img,
-    new_price: 85.0,
+    new_price: 5000,
+    description:
+      "Our helpers can perform regular weekly cleaning for your home, serving as an alternative to having a live-in maid or offering additional support to your household help team. This package comprises four (4) visits to your home and remains valid for one month from the date of purchase or the initial visit.",
   },
   {
     id: 6,
-    name: "Aircon Cleaning Service",
+    name: "Office Cleaning Service",
     category: "services",
     image: p6_img,
-    new_price: 85.0,
+    new_price: 2200,
+    description:
+      "Deep cleaning, sanitizing, and disinfecting for a clean and healthy workspace. Our business cleaning service is tailored to fit your business’ needs to ensure a sanitized and safe workspace. Our professional, commercial cleaners are well-equipped to thoroughly clean commercial spaces of all shapes and sizes. ",
   },
   {
     id: 7,
     name: "Home Organizing & Decluttering",
     category: "services",
     image: p7_img,
-    new_price: 85.0,
+    new_price: 2100,
+    description:
+      "A completely customized organizational system crafted to match your lifestyle. This involves a meticulous cleaning and disinfection of your space, along with the proper disposal of unwanted items to make space for new belongings. ",
   },
   {
     id: 8,
     name: "Steam Cleaning",
     category: "services",
     image: p8_img,
-    new_price: 85.0,
-    old_price: 120.5,
+    new_price: 2300,
+    description:
+      "Utilizes a Dry Steam cleaning device to naturally clean and disinfect hard surfaces without relying on chemicals. Eliminates undesired dust, stains, germs, bacteria, mildew, and mold from various surfaces such as tiles, grout, bath tubs, sink areas, shower enclosures, and fixtures.  ",
   },
 
   {
@@ -76,7 +91,6 @@ let all_product = [
     category: "products",
     image: p9_img,
     new_price: 85.0,
-    old_price: 120.5,
   },
   {
     id: 10,
@@ -84,7 +98,6 @@ let all_product = [
     category: "products",
     image: p10_img,
     new_price: 85.0,
-    old_price: 120.5,
   },
   {
     id: 11,
@@ -92,7 +105,6 @@ let all_product = [
     category: "products",
     image: p11_img,
     new_price: 85.0,
-    old_price: 120.5,
   },
   {
     id: 12,
@@ -100,7 +112,6 @@ let all_product = [
     category: "products",
     image: p12_img,
     new_price: 85.0,
-    old_price: 120.5,
   },
 ];
 
