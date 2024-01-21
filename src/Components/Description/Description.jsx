232141db7c15532f1b7a9c801c097a1aad6a1a41
jayannbal <jayannangela.balatero@tup.edu.ph>
2024-01-21 22:26:28 +0800

    update description file

--- src/Components/Description/Description.jsx
+++ src/Components/Description/Description.jsx
@@ -6,12 +6,12 @@ const Description = () => {
   return (
     <div className="description">
       <div className="text-desc">
-        <h1>We clean deeply so you can rest easy</h1>
+        <h1>The Best Cleaners Are Ready For Your Home</h1>
         <p>
-          We believe there’s nothing like coming home to a clean home. As a
-          professional home cleaning service company, we are dedicated to
-          helping un-mess people’s lives so you can focus on what matters most
-          to you.
+          We're firm believers that nothing beats the comfort of walking into a
+          freshly cleaned home. As a dedicated home cleaning service, our aim is
+          to simplify people's lives, giving you the freedom to focus on what
+          truly matters to you.
         </p>
         <button className="click-button">Read More...</button>
       </div>
